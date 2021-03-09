c101eb352ef7c3a7a607a76d3f5a54f187b78c8b
Shalom Acacio <shalom.acacio@gmail.com>
2021-03-09 01:50:44 -0300

    index principal

--- resources/views/pacientes/index.blade.php
+++ resources/views/pacientes/index.blade.php
@@ -5,7 +5,7 @@
     <div class="app-wrapper">
         <div class="app-content pt-3 p-md-3 p-lg-4">
             <div class="container-xl">
-                <h1 class="app-page-title">Parabéns, cadastro realizado com sucesso!</h1>
+                <h1 class="app-page-title">Vacinação COVID-19</h1>
 
                 <div class="row g-4">
 {{--                     
